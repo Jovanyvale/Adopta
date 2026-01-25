6460114f3fe00e6124aba2a0934f9d9908f2b88b
JovanyVale <gambino84_@hotmail.com>
2026-01-25 05:42:11 -0700

    Logout popup

--- app/(protected)/control-panel/layout.tsx
+++ app/(protected)/control-panel/layout.tsx
@@ -1,35 +1,70 @@
 'use client'
-import Image from "next/image"
-import Link from "next/link"
-import { useRouter } from "next/navigation";
-import { usePathname } from "next/navigation";
 import ControlPanelNavItem from "@/app/components/ControlPanelNavItem";
+import { useState } from "react";
 
 export default function ControlPanelLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
 
-    const router = useRouter();
-    const pathName = usePathname();
+    const [popUp, setPopUp] = useState<boolean>(false)
+
+    function handleClick(param: boolean) {
+        setPopUp(param)
+    }
+
+
 
     return (
-        <div className="flex h-[calc(100vh-128px)] mt-6">
-            <aside className="w-[22%] hidden md:flex flex-col bg-neutral-200 rounded-tr-lg">
+        <>
+            {/* Pop up code */}
+            {
+                popUp &&
+                <div className="fixed  w-screen h-screen bg-black/40 z-20 flex items-center justify-center">
+                    <div className="w-130 p-12 flex-col items-center justify-center mb-40 bg-white rounded-lg">
+                        <p className="text-lg text-center pb-5">Do you want to logout?</p>
+                        <div className="flex gap-6">
+                            <button className="p-2 rounded-lg bg-neutral-400 text-black hover:cursor-pointer w-full" onClick={() => handleClick(false)}>
+                                Cancel
+                            </button>
+                            <form action="/auth/logout" method="post" className="w-full">
+                                <button className="p-2 rounded-lg bg-black text-white hover:cursor-pointer w-full">
+                                    Logout
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            }
 
-                {/* Control panel */}
-                <ControlPanelNavItem name="Control panel" image="/icons/control-panel/control-panel-icon.svg" route="/control-panel" />
+            {/* Base code */}
+            < div className="flex h-[calc(100vh-128px)] mt-6" >
+                <aside className="w-[22%] hidden md:flex flex-col justify-between bg-neutral-200 rounded-tr-lg">
 
-                {/* Profile */}
-                <ControlPanelNavItem name="Profile" image="/icons/control-panel/profile-icon.svg" route="/control-panel/profile" />
+                    <div>
+                        {/* Control panel */}
+                        <ControlPanelNavItem name="Control panel" image="/icons/control-panel/control-panel-icon.svg" route="/control-panel" />
 
-                {/* Pets */}
-                <ControlPanelNavItem name="Pets" image="/icons/control-panel/pet-icon.svg" route="/control-panel/pets" />
+                        {/* Profile */}
+                        <ControlPanelNavItem name="Profile" image="/icons/control-panel/profile-icon.svg" route="/control-panel/profile" />
 
-                {/* Services */}
-                <ControlPanelNavItem name="Services" image="/icons/control-panel/services-icon.svg" route="/control-panel/services" />
-            </aside>
+                        {/* Pets */}
+                        <ControlPanelNavItem name="Pets" image="/icons/control-panel/pet-icon.svg" route="/control-panel/pets" />
 
-            <main className="flex-1">
-                {children}
-            </main>
-        </div >
+                        {/* Services */}
+                        <ControlPanelNavItem name="Services" image="/icons/control-panel/services-icon.svg" route="/control-panel/services" />
+                    </div>
+
+                    {/* Logut button */}
+                    <div className="flex my-5 mx-4">
+
+                        <button className="p-2 rounded-lg bg-black text-white w-full h-14 hover:cursor-pointer" onClick={() => handleClick(true)}>
+                            Logout
+                        </button>
+                    </div>
+                </aside>
+
+                <main className="flex-1">
+                    {children}
+                </main>
+            </div >
+        </>
     )
 }
